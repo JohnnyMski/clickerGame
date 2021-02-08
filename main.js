214e5f822c8e0614bfdd0b1e7782f44e04bcc2de
JohnnyMski <30510851+JohnnyMski@users.noreply.github.com>
2021-02-08 12:38:47 -0600

    idle gain and semicolons

--- main.js
+++ main.js
@@ -7,58 +7,63 @@ var gameData = {
     stuffUpgradeAmount: 0,
     stuffMachineCost: 50,
     stuffMachineAmount: 0,
-    stuffMachineOutput: 1
+    stuffMachineOutput: 1,
+    idleStuff: 0
 }
 
 function getStuff(amount) {
     if (amount == 0) {
-        gameData.stuff += gameData.stuffPerClick
+        gameData.stuff += gameData.stuffPerClick;
     }
     else {
-        gameData.stuff += gameData.stuffMachineAmount * gameData.stuffMachineOutput
+        gameData.stuff += gameData.idleStuff;
     }
     
-    document.getElementById("stuffGot").innerHTML = gameData.stuff + " Stuff got"
+    document.getElementById("stuffGot").innerHTML = gameData.stuff + " Stuff got";
+    document.getElementById("upgradeStuff").innerHTML = "Practice Stuff-Getting Technique (" + gameData.stuffUpgradeAmount + "). Cost: " + gameData.stuffUpgradeCost + " stuff";
+    document.getElementById("getMachine").innerHTML = "Build a Stuff-Gathering Machine out of Stuff (" + gameData.stuffMachineAmount + "). Cost: " + gameData.stuffMachineCost + " Stuff";
 }
 
 function upgradeStuff() {
     if (gameData.stuff >= gameData.stuffUpgradeCost) {
-        gameData.stuff -= gameData.stuffUpgradeCost
-        document.getElementById("stuffGot").innerHTML = gameData.stuff + " Stuff got"
-        gameData.stuffUpgradeAmount++
-        gameData.stuffPerClick++
-        gameData.stuffUpgradeCost *= 2
-        document.getElementById("upgradeStuff").innerHTML = "Practice Stuff-Getting Technique (" + gameData.stuffUpgradeAmount + "). Cost: " + gameData.stuffUpgradeCost + " Stuff"
-        localStorage.setItem("stuffClickerSave", JSON.stringify(gameData))
+        gameData.stuff -= gameData.stuffUpgradeCost;
+        document.getElementById("stuffGot").innerHTML = gameData.stuff + " Stuff got";
+        gameData.stuffUpgradeAmount++;
+        gameData.stuffPerClick++;
+        gameData.stuffUpgradeCost *= 2;
+        document.getElementById("upgradeStuff").innerHTML = "Practice Stuff-Getting Technique (" + gameData.stuffUpgradeAmount + "). Cost: " + gameData.stuffUpgradeCost + " Stuff";
+        localStorage.setItem("stuffClickerSave", JSON.stringify(gameData));
     }
     
 }
 
 function buyStuffMachine() {
     if (gameData.stuff >= gameData.stuffMachineCost) {
-        gameData.stuff -= gameData.stuffMachineCost
-        document.getElementById("stuffGot").innerHTML = gameData.stuff + " Stuff got"
-        gameData.stuffMachineAmount++
-        gameData.stuffMachineCost *= 2
-        document.getElementById("getMachine").innerHTML = "Build a Stuff-Gathering Machine out of Stuff (" + gameData.stuffMachineAmount + "). Cost: " + gameData.stuffMachineCost + " Stuff"
-        localStorage.setItem("stuffClickerSave", JSON.stringify(gameData))
+        gameData.stuff -= gameData.stuffMachineCost;
+        document.getElementById("stuffGot").innerHTML = gameData.stuff + " Stuff got";
+        gameData.stuffMachineAmount++;
+        gameData.stuffMachineCost *= 2;
+        gameData.idleStuff += gameData.stuffMachineOutput * gameData.stuffMachineAmount;
+        document.getElementById("getMachine").innerHTML = "Build a Stuff-Gathering Machine out of Stuff (" + gameData.stuffMachineAmount + "). Cost: " + gameData.stuffMachineCost + " Stuff";
+        localStorage.setItem("stuffClickerSave", JSON.stringify(gameData));
     }
 }
 
 var mainGameLoop = window.setInterval(function () {
-    getStuff(1)
-    document.getElementById("upgradeStuff").innerHTML = "Practice Stuff-Getting Technique (" + gameData.stuffUpgradeAmount + "). Cost: " + gameData.stuffUpgradeCost + " stuff"
-    document.getElementById("getMachine").innerHTML = "Build a Stuff-Gathering Machine out of Stuff (" + gameData.stuffMachineAmount + "). Cost: " + gameData.stuffMachineCost + " Stuff"
+    diff = Date.now() - gameData.lastTick;
+    gameData.lastTick = Date.now();
+    gameData.stuff += gameData.idleStuff * (diff / 1000);
+    getStuff(1);
 }, 1000)
 
 var saveGameLoop = window.setInterval(function () {
-    localStorage.setItem("stuffClickerSave", JSON.stringify(gameData))
+    localStorage.setItem("stuffClickerSave", JSON.stringify(gameData));
 }, 15000)
 
-var savegame = JSON.parse(localStorage.getItem("stuffClickerSave"))
+var savegame = JSON.parse(localStorage.getItem("stuffClickerSave"));
 if (savegame !== null) {
     if (gameData.update == 0) {
-        gameData = savegame
+        gameData = savegame;
     }
     
 }
